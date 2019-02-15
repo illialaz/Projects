007c44fb72b3db144464634ac6294f3f8a6ad7b7
illialaz <illia.laziuk2@gmail.com>
2019-02-15 21:42:14 +0300

    Create copy_replace.c
    - created copy_replace.c program to replace control characters
      from stdin to stdout:
      - backspace -> \b
      - tab -> \t
      - new line -> \n
      - \ -> \\
    - modified hello.c to test copy_replace.c

---
+++ byte/copy_replace.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+int main() {
+    int c;
+
+    while ((c = getchar()) != EOF) {
+        if (c == '\t') {
+            printf("\\t");
+        } else if (c == '\b') {
+            printf("\\b");
+        } else if (c == '\\') {
+            printf("\\\\");
+        } else if (c == '\n') {
+            printf("\\n");
+        } else {
+            putchar(c);
+        }
+    }
+    printf("\n");
+}
+
