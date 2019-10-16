b3db4bc2ffcc35966122d27dccddfe8d576fc5ba
illialaz <illia.laziuk2@gmail.com>
2019-10-17 00:58:44 +0300

    Renamed folders and added tasks from the first c++ cource

---
+++ cpp/lab3_Laziuk_Illia_7/cmake-build-debug/CMakeFiles/lab3_Laziuk_Illia_7.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab3_Laziuk_Illia_7.dir/quick_sort.cpp.o"
+  "lab3_Laziuk_Illia_7"
+  "lab3_Laziuk_Illia_7.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab3_Laziuk_Illia_7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
